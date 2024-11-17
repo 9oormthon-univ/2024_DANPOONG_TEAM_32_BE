b993c8332dd6de9c7e22e52a4eb6a3a186216973
Leehyundong <hyundongl@icloud.com>
2024-11-18 00:13:20 +0900

    initial commit

---
+++ src/main/java/com/danpoong/onchung/OnchungApplication.java
@@ -0,0 +1,13 @@
+package com.danpoong.onchung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnchungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnchungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danpoong/onchung/OnchungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danpoong.onchung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnchungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
