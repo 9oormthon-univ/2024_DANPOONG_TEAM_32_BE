be4d4392b382bd000e1736fe53ccba5e6ac895d8
Leehyundong <hyundongl@icloud.com>
2024-11-19 19:38:22 +0900

    feat :: 컨트롤러에 swagger 어노테이션 추가, yml 파일 업데이트

--- src/main/java/com/danpoong/onchung/domain/public_office/controller/PublicOfficeController.java
+++ src/main/java/com/danpoong/onchung/domain/public_office/controller/PublicOfficeController.java
@@ -4,6 +4,8 @@ import com.danpoong.onchung.domain.public_office.dto.FindAroundPublicOfficeReque
 import com.danpoong.onchung.domain.public_office.dto.FindAroundPublicOfficeResponse;
 import com.danpoong.onchung.domain.public_office.service.PublicOfficeService;
 import com.danpoong.onchung.global.template.ResponseTemplate;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +18,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/public-offices")
 @RequiredArgsConstructor
+@Tag(name = "Public Office Controller", description = "Public Office API")
 public class PublicOfficeController {
     private final PublicOfficeService publicOfficeService;
 
+    @Operation(summary = "근처 관공서 반환")
     @GetMapping("/nearby")
     public ResponseTemplate<List<FindAroundPublicOfficeResponse>> nearbyPublicOffices(@RequestBody FindAroundPublicOfficeRequest request) {
         return new ResponseTemplate<>(HttpStatus.OK, "근처 관공서 조회 성공", publicOfficeService.findAroundPublicOffice(request));
