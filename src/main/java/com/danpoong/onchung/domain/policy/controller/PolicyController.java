21b486f7357050d4e0aaa2bdd81cafb742d80785
Leehyundong <hyundongl@icloud.com>
2024-11-22 16:52:19 +0900

    feat :: 최근 본 복지, 즐겨찾기 기능 추가

--- src/main/java/com/danpoong/onchung/domain/policy/controller/PolicyController.java
+++ src/main/java/com/danpoong/onchung/domain/policy/controller/PolicyController.java
@@ -4,12 +4,12 @@ import com.danpoong.onchung.domain.policy.dto.PolicyResponseDto;
 import com.danpoong.onchung.domain.policy.service.PolicyService;
 import com.danpoong.onchung.global.template.ResponseTemplate;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,11 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class PolicyController {
     private final PolicyService policyService;
 
-    @Operation(summary = "정책 단일 조회")
+    @Operation(summary = "정책 단일 조회", description = "조회 시 최근 본 정책에 자동 추가")
     @GetMapping("/{policy_id}")
-    public ResponseTemplate<PolicyResponseDto> getPolicy(@PathVariable("policy_id") Long policyId) {
-        return new ResponseTemplate<>(HttpStatus.OK, "정책 단일 조회 성공", policyService.getPolicy(policyId));
+    public ResponseTemplate<PolicyResponseDto> getPolicy(@PathVariable("policy_id") Long policyId, HttpServletRequest request, HttpServletResponse response) {
+        return new ResponseTemplate<>(HttpStatus.OK, "정책 단일 조회 성공", policyService.getPolicy(policyId, request, response));
     }
 
+    @Operation(summary = "정책 즐겨찾기", description = "정책이 존재하면 삭제, 아니라면 추가")
+    @PatchMapping("/favorite/{policy_id}")
+    public ResponseTemplate<?> favoritePolicy(
+            @AuthenticationPrincipal Long userId,
+            @PathVariable("policy_id") Long policyId
+    ) {
+        policyService.favoritePolicy(userId, policyId);
+        return new ResponseTemplate<>(HttpStatus.OK, "즐겨찾기 추가 / 삭제 성공");
+    }
 
 }
