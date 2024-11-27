08f0ce75348d6f54faccc288821eba9ab246ce88
Leehyundong <hyundongl@icloud.com>
2024-11-27 17:31:40 +0900

    feat :: 사용자 닉네임 기능 추가

--- src/main/java/com/danpoong/onchung/global/security/oauth/kakao/KakaoUserInfo.java
+++ src/main/java/com/danpoong/onchung/global/security/oauth/kakao/KakaoUserInfo.java
@@ -3,39 +3,20 @@ package com.danpoong.onchung.global.security.oauth.kakao;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
-@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class KakaoUserInfo {
     @JsonProperty("kakao_account")
     private KakaoAccount kakaoAccount;
 
     @Getter
-    @ToString
     @JsonIgnoreProperties(ignoreUnknown = true)
     static class KakaoAccount {
-        private KakaoProfile profile;
         private String email;
-    }
-
-    @Getter
-    @ToString
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    static class KakaoProfile {
-        private String nickname;
     }
 
     public String getEmail() {
         return this.kakaoAccount.email;
-    }
-
-//    public String getName() {
-//        return kakaoAccount.name;
-//    }
-
-    public String getNickname() {
-        return this.kakaoAccount.profile.nickname;
     }
 }
