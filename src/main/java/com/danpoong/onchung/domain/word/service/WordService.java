ebb8237e1276bcc8c52eb843a64903ac211f2ab4
yuj2029@gmail.com <yuj2029@gmail.com>
2024-11-30 01:06:30 +0900

    fix :: 청년 시사경제 용어 사전 조회 기능 수정

--- src/main/java/com/danpoong/onchung/domain/word/service/WordService.java
+++ src/main/java/com/danpoong/onchung/domain/word/service/WordService.java
@@ -1,8 +1,7 @@
 package com.danpoong.onchung.domain.word.service;
 
-//import com.danpoong.onchung.domain.user.domain.UserInfo;
-//import com.danpoong.onchung.domain.user.repository.UserRepository;
-
+import com.danpoong.onchung.domain.user.domain.UserInfo;
+import com.danpoong.onchung.domain.user.repository.UserInfoRepository;
 import com.danpoong.onchung.domain.word.domain.Word;
 import com.danpoong.onchung.domain.word.domain.enums.WordCategory;
 import com.danpoong.onchung.domain.word.dto.WordResponseDto;
@@ -10,6 +9,7 @@ import com.danpoong.onchung.domain.word.dto.WordSummaryResponseDto;
 import com.danpoong.onchung.domain.word.repository.WordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,22 +22,26 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class WordService {
     private final WordRepository wordRepository;
-//    private final UserRepository userRepository;
+    private final UserInfoRepository userInfoRepository;
 
     private static final int WORD_PAGE_SIZE = 10;
 
-    public Page<WordSummaryResponseDto> getWordsByCategory(String category, int page) {
-        Page<Word> words = wordRepository.findByCategory(WordCategory.checkCategory(category), PageRequest.of(page, WORD_PAGE_SIZE));
+    public Page<WordSummaryResponseDto> getWordsByCategory(String category, Long id, int page) {
+        Page<Word> words = wordRepository.findWordsByCategoryWithBookmarkFirst(
+                WordCategory.checkCategory(category),
+                id,
+                PageRequest.of(page, WORD_PAGE_SIZE)
+        );
 
         return words.map(word -> {
             boolean isBookmarked = false;
 
-//            if (userId != null) {
-//                UserInfo userInfo = userRepository.findById(userId)
-//                        .orElseThrow(() -> new RuntimeException("해당 ID의 사용자가 존재하지 않습니다."));
-//
-//                isBookmarked = userInfo.getFavoriteWords().contains(word);
-//            }
+            if (id != null) {
+                UserInfo userInfo = userInfoRepository.findById(id)
+                        .orElseThrow(() -> new RuntimeException("해당 ID의 사용자가 존재하지 않습니다."));
+
+                isBookmarked = userInfo.getFavoriteWords().contains(word);
+            }
 
             return WordSummaryResponseDto.builder()
                     .wordId(word.getId())
@@ -60,81 +64,81 @@ public class WordService {
                 .build();
     }
 
-//    @Transactional
-//    public void favoriteWord(Long userId, Long wordId) {
-//        UserInfo userInfo = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("사용자가 존재하지 않습니다."));
-//        Word word = wordRepository.findById(wordId).orElseThrow(() -> new RuntimeException("해당 ID의 단어가 존재하지 않습니다."));
-//
-//        boolean isPresent = userInfo.getFavoriteWords().contains(wordId);
-//
-//        if (!isPresent) {
-//            userInfo.addFavoriteWord(word);
-//        } else {
-//            userInfo.removeFavoriteWord(word);
-//        }
-//    }
+    @Transactional
+    public void favoriteWord(Long id, Long wordId) {
+        UserInfo userInfo = userInfoRepository.findById(id).orElseThrow(() -> new RuntimeException("사용자가 존재하지 않습니다."));
+        Word word = wordRepository.findById(wordId).orElseThrow(() -> new RuntimeException("해당 ID의 단어가 존재하지 않습니다."));
 
-//    public Page<WordSummaryResponseDto> getBookmarkedWords(Long userId, int page) {
-//        UserInfo userInfo = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("해당 ID의 사용자가 존재하지 않습니다."));
-//        List<Word> wordList = userInfo.getFavoriteWords();
-//
-//        int totalWords = wordList.size();
-//        int start = Math.min(page * WORD_PAGE_SIZE, totalWords);
-//        int end = Math.min((page + 1) * WORD_PAGE_SIZE, totalWords);
-//
-//        List<Word> pagedWords = wordList.subList(start, end);
-//        Page<Word> pageResult = new PageImpl<>(pagedWords, PageRequest.of(page, WORD_PAGE_SIZE), totalWords);
-//
-//        return pageResult.map(word -> WordSummaryResponseDto.builder()
-//                .wordId(word.getId())
-//                .term(word.getTerm())
-//                .isBookmark(true)
-//                .relatedWelfare(word.getRelatedWelfare())
-//                .build()
-//        );
-//    }
+        boolean isPresent = userInfo.getFavoriteWords().contains(word);
 
-    public WordSummaryResponseDto searchWord(String type, String term) {
-//        UserInfo userInfo = userRepository.findById(userId)
-//                .orElseThrow(() -> new RuntimeException("해당 ID의 사용자가 존재하지 않습니다."));
+        if (!isPresent) {
+            userInfo.addFavoriteWord(word);
+        } else {
+            userInfo.removeFavoriteWord(word);
+        }
+    }
+
+    public Page<WordSummaryResponseDto> getBookmarkedWords(Long id, int page) {
+        UserInfo userInfo = userInfoRepository.findById(id).orElseThrow(() -> new RuntimeException("해당 ID의 사용자가 존재하지 않습니다."));
+        List<Word> wordList = userInfo.getFavoriteWords();
+
+        int totalWords = wordList.size();
+        int start = Math.min(page * WORD_PAGE_SIZE, totalWords);
+        int end = Math.min((page + 1) * WORD_PAGE_SIZE, totalWords);
+
+        List<Word> pagedWords = wordList.subList(start, end);
+        Page<Word> pageResult = new PageImpl<>(pagedWords, PageRequest.of(page, WORD_PAGE_SIZE), totalWords);
+
+        return pageResult.map(word -> WordSummaryResponseDto.builder()
+                .wordId(word.getId())
+                .term(word.getTerm())
+                .isBookmark(true)
+                .relatedWelfare(word.getRelatedWelfare())
+                .build()
+        );
+    }
+
+    public WordSummaryResponseDto searchWord(String type, String term, Long id) {
+        UserInfo userInfo = userInfoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("해당 ID의 사용자가 존재하지 않습니다."));
 
         Word word = wordRepository.findByTerm(term).orElse(null);
         if (word == null) {
             return WordSummaryResponseDto.empty();
         }
 
-//        // 북마크 검색
-//        if (type.equals("북마크")) {
-//            return searchBookmark(userInfo, word);
-//        }
-//
+        // 북마크 검색
+        if (type.equals("북마크")) {
+            return searchBookmark(userInfo, word);
+        }
+
         // 전체 검색
         if (type.isEmpty()) {
-            return searchAll(word);
+            return searchAll(word, userInfo);
         }
 
         // 카테고리 검색
-        return searchByCategory(type, word);
+        return searchByCategory(type, word, userInfo);
     }
 
     // 북마크 내 검색
-//    private WordSummaryResponseDto searchBookmark(UserInfo userInfo, Word word) {
-//        for (Word favoriteWord : userInfo.getFavoriteWords()) {
-//            if (favoriteWord.getTerm().equals(word.getTerm())) {
-//                return createWordSummaryResponse(favoriteWord, true);
-//            }
-//        }
-//        return WordSummaryResponseDto.empty();
-//    }
+    private WordSummaryResponseDto searchBookmark(UserInfo userInfo, Word word) {
+        for (Word favoriteWord : userInfo.getFavoriteWords()) {
+            if (favoriteWord.getTerm().equals(word.getTerm())) {
+                return createWordSummaryResponse(favoriteWord, true);
+            }
+        }
+        return WordSummaryResponseDto.empty();
+    }
 
     // 전체 검색
-    private WordSummaryResponseDto searchAll(Word word) {
-//        boolean isBookmark = userInfo.getFavoriteWords().contains(word);
+    private WordSummaryResponseDto searchAll(Word word, UserInfo userInfo) {
+        boolean isBookmark = userInfo.getFavoriteWords().contains(word);
         return createWordSummaryResponse(word, false);
     }
 
     // 카테고리 내 검색
-    private WordSummaryResponseDto searchByCategory(String type, Word word) {
+    private WordSummaryResponseDto searchByCategory(String type, Word word, UserInfo userInfo) {
         WordCategory wordCategoryEng = WordCategory.checkCategory(type);
         List<Word> wordList = wordRepository.findAllByCategory(wordCategoryEng);
 
